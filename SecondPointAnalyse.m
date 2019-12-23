eaecc8c8e33cbace056389aa5bc1be8854074db4
NewKid <newkidlp2@gmail.com>
2019-12-23 05:37:50 +0300

    Ex

--- SecondPointAnalyse.m
+++ SecondPointAnalyse.m
@@ -2,7 +2,8 @@ hold on;
 grid on;
 for b = 0.1 : 0.1 : 3
     for c = 0.1 : 0.1 : 1.5
-        if (abs((c + sqrt(c^2 - 4 * b * (1 - 2*(b + c - 1)/b))) / 2) < 1) && (abs((c - sqrt(c^2 - 4 * b * (1 - 2*(b + c - 1)/b))) / 2) < 1)
+        if (abs((c + sqrt(c^2 - 4 * b * (1 - 2 * (b + c - 1) / b))) / 2) < 1)...
+            && (abs((c - sqrt(c^2 - 4 * b * (1 - 2 * (b + c - 1) / b))) / 2) < 1)
             plot(b, c, "g.");
         else
             plot(b, c, "r.");
